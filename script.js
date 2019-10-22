3a88813b0d667842b2cf3a0a282a6e85db79120b
nunezrio <nunezrio@gmail.com>
2019-10-22 08:36:18 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// {
+//   let letters = ["a", "b", "c", "d", "e"];
+//   //Loop through an array using for
+//   for (let i = 0; i < letters.length; i++) {
+//     let letter = letters[1];
+//     console.log(letters[i]);
+//   }
+//   //loop through array using for...of
+//   for (const letter of letters) {
+//     console.log(letter);
+//   }
+// }
+
+let name = "Rion Nunez";
+let age = 33;
+let birthday = "July 11";
+let detroitGC = true;
+let lifeEvents = [
+  "I was born in Houston, Texas.",
+  "I grew up in Southfield, Michigan.",
+  "I graduated from Michigan Stage University.",
+  "I'm currently enrolled in the front-end bootcamp."
+];
+
+if (detroitGC === true) {
+  console.log(
+    `My name is ${name} and I am a student at Grand Circus in Detroit, Michigan. I am currently ${age} years old and my birthday is on ${birthday}`
+  );
+} else {
+  console.log(
+    `My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`
+  );
+}
+for (i = 0; i < lifeEvents.length; i++) {
+  console.log(lifeEvents[i]);
+}
+
+
+
+let counter = 0;
+while (random !== 5; counter++)
+
+
+
+let randomNumber = Math.round(Math.random() * 10;
+if (randomNumber !== 5; counter++) {
+  console.log(`${randomNumber} !==5`)
+} else (counter++) {
+console.log(`5===5`);
+}
